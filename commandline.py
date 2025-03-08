63bb049eed0065e99c534326510680cd27708878
Your Name <you@example.com>
2025-03-08 12:40:13 -0500

    Updated the font

--- commandline.py
+++ commandline.py
@@ -5,8 +5,26 @@ label = sg.Text('Type in a to-do')
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My To Do App', layout=[[label], [input_box, add_button]], font=('Helvetica', 20))
 window.read()
 window.close()
+
+#How to push your code to github
+# connect to your repository
+#git remote add origin https://github.com/your-username/your-repo.git
+
+# track all files
+# git add .
+
+# commit your changes
+# git commit -m "Initial commit"
+
+# if pushing for the first time
+# git branch -M main
+# git push -u origin main
+
+# for the subsequent time
+# git push
+
 
 
