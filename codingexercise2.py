53b24a2b474b15af9c13eda741643f08dfe55db4
Your Name <you@example.com>
2025-03-08 18:34:21 -0500

    Update

--- codingexercise2.py
+++ codingexercise2.py
@@ -1,13 +1,22 @@
 import FreeSimpleGUI as sg
+from Converter import convert
 
 label1 = sg.Text("Enter feet")
-input1 = sg.Input()
+input1 = sg.Input(key='feet')
 
 label2 = sg.Text("Enter inches")
-input2 = sg.Input()
+input2 = sg.Input(key='incehs')
 
 convert_button = sg.Button("Convert")
 
-window = sg.Window("Convertor", layout=[[label1, input1], [label2, input2], [convert_button]])
-window.read()
+output_label = sg.Text("", key='result')
+
+window = sg.Window("Convertor", layout=[[label1, input1], [label2, input2], [convert_button, output_label]])
+
+while True:
+    event, values = window.read()
+    feet = float(values['feet']) 
+    inches = float(values['incehs'])
+    result = convert(feet, inches)
+    window['result'].update(value=result)
 window.close()
