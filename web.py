0607407a55bc0e00ea0685f40f8e1a00f568022a
Your Name <you@example.com>
2025-03-15 12:30:22 -0400

    create a static web interface

--- web.py
+++ web.py
@@ -1,7 +1,15 @@
 import streamlit as st
+from decorator import append
+
 import functions
 
 todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
 
 st.title("My Todo APP")
 st.subheader("This is my to do APP.")
@@ -10,6 +18,7 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo here...")
+st.text_input(label="", placeholder="Add a new todo here...", on_change=add_todo,
+              key="new_todo")
 
 
